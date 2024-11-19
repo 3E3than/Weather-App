8d44345bb74cd9086fbb43db53be5f11aa94ec8d
3E3than <112522726+3E3than@users.noreply.github.com>
2024-11-19 15:17:44 -0800

    basic weather functionality

---
+++ src/components/Weather.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useRef, useState } from 'react'
+import './Weather.css'
+import search_icon from '../assets/search.png'
+import cloud_icon from '../assets/cloud.png'
+import drizzle_icon from '../assets/drizzle.png'
+import humidity_icon from '../assets/humidity.png'
+import rain_icon from '../assets/rain.png'
+import snow_icon from '../assets/snow.png'
+import wind_icon from '../assets/wind.png'
+import clear_icon from '../assets/clear.png'
+
+const Weather = () => {
+
+    const [weatherData, setWeatherData] = useState(false);
+    const inputRef = useRef();
+
+
+    const search = async (city)=>{
+        if (city == "") {
+            alert("enter city name")
+            return;
+        }
+        try {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+            const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` || clear_icon;
+            setWeatherData({
+                humidity: data.main.humidity, 
+                windspeed: data.wind.speed, 
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                icon: icon
+            })
+        }
+        catch(error) {
+            setWeatherData(false);
+            alert("incorrect city")
+            console.error("error fetching data")
+        }
+    }
+
+    useEffect(()=>{
+        search("Hsinchu");
+    },[])
+
+  return (
+    <div className = 'weather'>
+        <div className= 'searchbar'>
+            <input ref = {inputRef} type = 'text' placeholder= 'search' />
+            <img src = {search_icon} alt='' onClick={()=>search(inputRef.current.value)}/>
+        </div>
+        <img src = {weatherData.icon} className = 'weathericon'/>
+        <p className='temperature'>{weatherData.temperature} degrees C</p>
+        <p className='location'>{weatherData.location}</p>
+        <div className='weatherdata'>
+            <div className='col'>
+                <img src={wind_icon} alt=''/>
+                <div>
+                    <p>{weatherData.windspeed}</p>
+                    <span>Wind Speed</span>
+                </div>
+            </div>
+            <div className='col'>
+                <img src={humidity_icon} alt=''/>
+                <div>
+                    <p>{weatherData.humidity} %</p>
+                    <span>Humidity</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Weather
